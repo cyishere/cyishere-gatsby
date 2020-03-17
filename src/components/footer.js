a7013023a84cc23e39731dd94932e4f6ed20757b
cyishere <cyishere@gmail.com>
2020-03-17 17:08:57 +0800

    add twitter link to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <div className="uk-container uk-padding">
                 <div className="uk-align-left uk-text-small">&copy; 2020 Made with <span role="img" aria-label="heart">💖</span> by <Link to="/about">CY</Link> in Beijing.</div>
                 <div className="uk-align-right">
+                    <a className="uk-margin-small-right" href="https://twitter.com/cyishere" style={{ color: `#333`}}><i data-uk-icon="icon: twitter"></i></a>
                     <a href="https://github.com/cyishere" style={{ color: `#333`}}><i data-uk-icon="icon: github"></i></a>
                 </div>
             </div>
